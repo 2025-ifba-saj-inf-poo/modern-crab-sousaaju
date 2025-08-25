1f35706d094cb1d1899aa49caf19bbbbd7957508
lay-del <20241ti0043@ifba.edu.br>
2025-08-25 11:41:32 -0300

    Fim decapoda

--- CrabWorld.java
+++ CrabWorld.java
@@ -16,5 +16,114 @@ public class CrabWorld extends World
     public CrabWorld()
     {    
         super(560, 560, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Crab crab = new Crab();
+        addObject(crab,117,112);
+        Worm worm = new Worm();
+        addObject(worm,145,380);
+        Worm worm2 = new Worm();
+        addObject(worm2,338,398);
+        Worm worm3 = new Worm();
+        addObject(worm3,432,338);
+        Worm worm4 = new Worm();
+        addObject(worm4,400,323);
+        Worm worm5 = new Worm();
+        addObject(worm5,347,188);
+        Worm worm6 = new Worm();
+        addObject(worm6,209,198);
+        Worm worm7 = new Worm();
+        addObject(worm7,291,146);
+        Worm worm8 = new Worm();
+        addObject(worm8,375,137);
+        Lobster lobster = new Lobster();
+        addObject(lobster,473,231);
+        Worm worm9 = new Worm();
+        addObject(worm9,271,287);
+        Worm worm10 = new Worm();
+        addObject(worm10,221,303);
+        Worm worm11 = new Worm();
+        addObject(worm11,132,227);
+        Worm worm12 = new Worm();
+        addObject(worm12,53,396);
+        Worm worm13 = new Worm();
+        addObject(worm13,80,405);
+        Worm worm14 = new Worm();
+        addObject(worm14,286,474);
+        Worm worm15 = new Worm();
+        addObject(worm15,358,464);
+        Worm worm16 = new Worm();
+        addObject(worm16,393,490);
+        Worm worm17 = new Worm();
+        addObject(worm17,170,438);
+        Worm worm18 = new Worm();
+        addObject(worm18,196,486);
+        Worm worm19 = new Worm();
+        addObject(worm19,180,355);
+        Worm worm20 = new Worm();
+        addObject(worm20,86,432);
+        Worm worm21 = new Worm();
+        addObject(worm21,58,285);
+        Worm worm22 = new Worm();
+        addObject(worm22,299,480);
+        Worm worm23 = new Worm();
+        addObject(worm23,98,507);
+        Worm worm24 = new Worm();
+        addObject(worm24,522,342);
+        Worm worm25 = new Worm();
+        addObject(worm25,504,415);
+        Worm worm26 = new Worm();
+        addObject(worm26,453,505);
+        Worm worm27 = new Worm();
+        addObject(worm27,337,99);
+        Worm worm28 = new Worm();
+        addObject(worm28,455,103);
+        Worm worm29 = new Worm();
+        addObject(worm29,522,156);
+        Worm worm30 = new Worm();
+        addObject(worm30,485,124);
+        Worm worm31 = new Worm();
+        addObject(worm31,449,84);
+        Worm worm32 = new Worm();
+        addObject(worm32,257,84);
+        Worm worm33 = new Worm();
+        addObject(worm33,316,42);
+        Worm worm34 = new Worm();
+        addObject(worm34,388,49);
+        Worm worm35 = new Worm();
+        addObject(worm35,453,65);
+        Worm worm36 = new Worm();
+        addObject(worm36,519,198);
+        Worm worm37 = new Worm();
+        addObject(worm37,247,56);
+        Worm worm38 = new Worm();
+        addObject(worm38,257,37);
+        Worm worm39 = new Worm();
+        addObject(worm39,274,39);
+        Worm worm40 = new Worm();
+        addObject(worm40,321,356);
+        Worm worm41 = new Worm();
+        addObject(worm41,339,312);
+        Worm worm42 = new Worm();
+        addObject(worm42,317,241);
+        Worm worm43 = new Worm();
+        addObject(worm43,240,260);
+        Worm worm44 = new Worm();
+        addObject(worm44,96,283);
+        Worm worm45 = new Worm();
+        addObject(worm45,39,227);
+        Worm worm46 = new Worm();
+        addObject(worm46,51,193);
+        Worm worm47 = new Worm();
+        addObject(worm47,198,129);
+        Worm worm48 = new Worm();
+        addObject(worm48,315,344);
     }
 }
